eabb4feaac743c9ba803ca1c2111e204dfbc3592
DesmondBOOM <870895580@qq.com>
2021-11-29 17:22:19 +0800

    [Zijian Liu] add lifecycle to LoginActivity and Log it

--- app/src/main/java/com/example/hello/LoginActivity.java
+++ app/src/main/java/com/example/hello/LoginActivity.java
@@ -3,12 +3,53 @@ package com.example.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "[LoginActivity]";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        Log.d(TAG, "on create");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "on start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "on resume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "on restart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "on pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "on stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "on destroy");
     }
 }
