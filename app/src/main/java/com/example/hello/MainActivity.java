68445218bd39a0d77de762ed61497404b8cec0c6
DesmondBOOM <870895580@qq.com>
2021-11-29 14:30:11 +0800

    [Zijian Liu] add TextView Hello

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.hello;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     public static final String TAG = "MainActivity";
 
@@ -12,8 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Log.d(TAG, "this is main activity");
+
+        button = (Button) findViewById(R.id.button_1);
+        button.setOnClickListener(v -> openConstraintLayout());
+    }
+
+    private void openConstraintLayout() {
+
     }
 
 }
