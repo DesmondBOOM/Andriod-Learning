c8b483476f9bc0d7f870196e907b2018d3b92299
DesmondBOOM <870895580@qq.com>
2021-12-01 14:02:03 +0800

    [Zijian Liu] add tweet adapter into activity

--- app/src/main/java/com/example/hello/RecyclerViewActivity.java
+++ app/src/main/java/com/example/hello/RecyclerViewActivity.java
@@ -1,14 +1,32 @@
 package com.example.hello;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.hello.adapter.TweetsAdapter;
+import com.example.hello.model.Tweet;
+
+import java.util.List;
+
 public class RecyclerViewActivity extends AppCompatActivity {
+
+    List<Tweet> tweetList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recycler_view_layout);
+
+        // Lookup the recyclerview in activity layout
+        RecyclerView tweetRecyclerView = findViewById(R.id.tweet_recycler);
+
+        TweetsAdapter adapter = new TweetsAdapter(tweetList);
+        tweetRecyclerView.setAdapter(adapter);
+        tweetRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
