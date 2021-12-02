8a41c7ed673febcf6bd14b92e60aafc988e383ea
DesmondBOOM <870895580@qq.com>
2021-12-02 15:54:41 +0800

    [Zijian Liu] refactor

--- app/src/main/java/com/example/hello/ThreadActivity.java
+++ app/src/main/java/com/example/hello/ThreadActivity.java
@@ -15,49 +15,56 @@ public class ThreadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.thread_layout);
-
         initUI();
     }
 
     private void initUI() {
         btnThread = findViewById(R.id.btn_thread);
-        btnThread.setOnClickListener(v -> startCounter());
+        btnThread.setOnClickListener(v -> initCounter());
+    }
+
+    private void initCounter() {
+        disableButton();
+        startCounter();
     }
 
     private void startCounter() {
+        Thread thread = new Thread(() -> {
+            Log.d("[counter]", "new thread: " + Thread.currentThread().getName());
+            for (int index = 0; index <= 10; ++index) {
+                setButtonText(index);
+                if (index != 10) {
+                    SystemClock.sleep(1000);
+                }
+            }
+            enableButton();
+        });
+        thread.start();
+    }
+
+    private void setButtonText(int index) {
+        runOnUiThread(() -> {
+            btnThread.setText(String.valueOf(index));
+            Log.d("[setButtonText]", "runOnUiThread: " + Thread.currentThread().getName());
+        });
+    }
+
+    private void disableButton() {
         btnThread.setEnabled(false);
         btnThread.setClickable(false);
         Log.d("[startCounter]", "init " + Thread.currentThread().getName());
         Log.d("[startCounter]", "Disable button");
         Log.d("[startCounter]", "btn enabled: " + btnThread.isEnabled());
         Log.d("[startCounter]", "btn clickable: " + btnThread.isClickable());
-
-        counter();
     }
 
-    private void counter() {
-        Thread thread = new Thread(() -> {
-            Log.d("[counter]", "new thread: " + Thread.currentThread().getName());
-
-            for (int index = 0; index <= 10; ++index) {
-                final Integer finalIndex = index;
-                runOnUiThread(() -> {
-                    btnThread.setText(String.valueOf(finalIndex));
-                    Log.d("[counter]", "runOnUiThread: " + Thread.currentThread().getName());
-                });
-                if (index != 10) {
-                    SystemClock.sleep(1000);
-                }
-            }
-
-            runOnUiThread(() -> {
-                btnThread.setEnabled(true);
-                btnThread.setClickable(true);
-                Log.d("[counter]", "Enable button");
-                Log.d("[counter]", "btn enabled: " + btnThread.isEnabled());
-                Log.d("[counter]", "btn clickable: " + btnThread.isClickable());
-            });
+    private void enableButton() {
+        runOnUiThread(() -> {
+            btnThread.setEnabled(true);
+            btnThread.setClickable(true);
+            Log.d("[counter]", "Enable button");
+            Log.d("[counter]", "btn enabled: " + btnThread.isEnabled());
+            Log.d("[counter]", "btn clickable: " + btnThread.isClickable());
         });
-        thread.start();
     }
 }
