fb2d434f7d407d036c43e7942b40edbe88194e50
DesmondBOOM <870895580@qq.com>
2021-12-06 20:32:41 +0800

    [Zijian Liu] refactor: remove duplicated method

--- app/src/main/java/com/example/hello/RecyclerViewActivity.java
+++ app/src/main/java/com/example/hello/RecyclerViewActivity.java
@@ -8,19 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hello.adapter.TweetsAdapter;
-import com.example.hello.data.model.Tweet;
 import com.example.hello.utils.Dependency;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
@@ -29,7 +17,6 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class RecyclerViewActivity extends AppCompatActivity {
 
-    private final Gson gson = new GsonBuilder().create();
     private TweetsAdapter tweetsAdapter;
     private Dependency dependency;
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
@@ -64,34 +51,5 @@ public class RecyclerViewActivity extends AppCompatActivity {
                         throwable -> Toast.makeText(RecyclerViewActivity.this, throwable.getMessage(), Toast.LENGTH_SHORT).show()
                 );
         compositeDisposable.add(disposable);
-
-//        List<Tweet> tweetList = getTweetsFromAssets();
-//        tweetList = tweetList.stream().filter(tweet -> tweet.getError() == null && tweet.getUnknownError() == null).collect(Collectors.toList());
-//        tweetsAdapter.setTweets(tweetList);
-    }
-
-    private List<Tweet> getTweetsFromAssets() {
-        List<Tweet> tweets = new ArrayList<>();
-
-        try {
-            InputStreamReader inputStreamReader = new InputStreamReader(getAssets().open("tweet_info.json"), StandardCharsets.UTF_8);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-            String line;
-            StringBuilder stringBuilder = new StringBuilder();
-            while((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line);
-            }
-            bufferedReader.close();
-            inputStreamReader.close();
-            String jsonString = stringBuilder.toString();
-
-            tweets = gson.fromJson(jsonString, new TypeToken<List<Tweet>>() {}.getType());
-            return tweets;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return tweets;
     }
 }
